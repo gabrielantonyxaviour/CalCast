e85a1000bfe07e54c967f9adf50ca0442c53708f
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2024-03-23 18:38:45 +0530

    Ui changes -frames

--- frontend/src/app/frames/route.tsx
+++ frontend/src/app/frames/route.tsx
@@ -5,14 +5,133 @@ import { frames } from "./frames";
 const handleRequest = frames(async (ctx) => {
   return {
     image: (
-      <span tw=" flex-col">
-        <span>CastCaL</span>
-        <span>Sheduling Infrastructure for Farcaster</span>
-      </span>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 20,
+          // justifyContent: "center",
+
+          width: "100%",
+          height: "100%",
+          backgroundColor: "black",
+          padding: 50,
+          fontSize: 24,
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <div
+              style={{
+                color: "white",
+                fontSize: 64,
+              }}
+            >
+              CalCast
+            </div>
+            <div
+              style={{
+                color: "white",
+                display: "flex",
+              }}
+            >
+              <img
+                src="https://calcast.vercel.app/calendar.png"
+                width={64}
+                height={64}
+                alt="calendar"
+              />
+            </div>
+          </div>
+          <div
+            style={{
+              color: "gray",
+              fontSize: 40,
+            }}
+          >
+            Scheduling Infrastructure for Farcaster
+          </div>
+        </div>
+        <div
+          style={{
+            color: "white",
+            display: "flex",
+            flexDirection: "column",
+            marginTop: 60,
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              gap: 10,
+              alignItems: "center",
+              justifyContent: "flex-end",
+            }}
+          >
+            <div
+              style={{
+                color: "white",
+                fontSize: 80,
+                marginBottom: 8,
+              }}
+            >
+              0xLeo
+            </div>
+            <hr
+              style={{
+                borderColor: "white",
+                width: 80,
+                transform: "rotate(-65deg)",
+                margin: 0,
+                borderWidth: 1,
+              }}
+            />
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <img
+                src="https://calcast.vercel.app/calendar.png"
+                alt="Circular Image"
+                style={{
+                  borderRadius: "50%",
+                  border: "1px solid white",
+                  width: 90,
+                  height: 90,
+                }}
+              />
+            </div>
+          </div>
+          <div
+            style={{
+              fontSize: 40,
+              marginTop: 8,
+              alignSelf: "flex-end",
+              color: "gray",
+            }}
+          >
+            Talks about Infrastructure and tooling
+          </div>
+        </div>
+      </div>
     ),
     buttons: [
       <Button action="post" target="/bookings">
-        Start
+        Schedule a call now!
       </Button>,
     ],
   };
