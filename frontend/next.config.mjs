fc31d4fb17ec91745068bf2258009bf1e5672709
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2024-03-23 17:32:38 +0530

    Added captcha

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["@resvg/resvg-js"],
+  },
+  webpack: (config) => {
+    config.externals.push({
+      sharp: "commonjs sharp",
+      "@resvg/resvg-js": "commonjs @resvg/resvg-js",
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
--- frontend/src/app/frames/bookings/route.tsx
+++ frontend/src/app/frames/bookings/route.tsx
@@ -1,7 +1,17 @@
+/* eslint-disable react/jsx-key */
 import { Button } from "frames.js/next";
 import { frames } from "../frames";
 import { getNextSixDates } from "@/lib/date";
 import { createTimeSlots } from "@/lib/time";
+import {
+  FrameRatio,
+  generateCaptchaChallenge,
+  GenerateCaptchaChallengeInput,
+  GenerateCaptchaChallengeOutput,
+  validateCaptchaChallenge,
+  ValidateCaptchaChallengeInput,
+  ValidateCaptchaChallengeOutput,
+} from "@airstack/frames";
 
 const handleRequest = frames(async (ctx) => {
   const booking = ctx.searchParams;
@@ -16,7 +26,8 @@ const handleRequest = frames(async (ctx) => {
             display: "flex",
             flexDirection: "column",
             gap: 20,
-            justifyContent: "center",
+            // justifyContent: "center",
+
             width: "100%",
             height: "100%",
             backgroundColor: "black",
@@ -28,6 +39,7 @@ const handleRequest = frames(async (ctx) => {
             style={{
               display: "flex",
               flexDirection: "column",
+              justifyContent: "center",
             }}
           >
             <div
@@ -40,7 +52,7 @@ const handleRequest = frames(async (ctx) => {
               <div
                 style={{
                   color: "white",
-                  fontSize: 48,
+                  fontSize: 64,
                 }}
               >
                 CalCast
@@ -48,15 +60,20 @@ const handleRequest = frames(async (ctx) => {
               <div
                 style={{
                   color: "white",
+                  display: "flex",
                 }}
               >
-                Logo
+                <img
+                  src="http://localhost:3000/calendar.png"
+                  width={64}
+                  height={64}
+                ></img>
               </div>
             </div>
             <div
               style={{
                 color: "gray",
-                fontSize: 20,
+                fontSize: 40,
               }}
             >
               Sheduling Infrastructure for Farcaster
@@ -68,24 +85,61 @@ const handleRequest = frames(async (ctx) => {
               display: "flex",
               flexDirection: "column",
               justifyContent: "space-between",
+              marginTop: 50,
             }}
           >
             <div
               style={{
-                fontSize: 20,
-                marginBottom: 8,
+                display: "flex",
+                gap: 10,
+                alignItems: "center",
               }}
             >
-              0xLeo
+              <div
+                style={{
+                  color: "white",
+                  fontSize: 40,
+                  marginBottom: 8,
+                }}
+              >
+                0xLeo
+              </div>
+              {/* <hr
+                style={{
+                  width: "15%", // Adjust the width of the line as needed
+                  margin: 0,
+                  borderWidth: 0.5,
+                  borderColor: "white",
+                  transform: "rotate(-49deg)", // Rotate the line to make it diagonal
+                }}
+              /> */}
+              <hr
+                style={{
+                  borderColor: "white",
+                  width: 50,
+                  transform: "rotate(-65deg)",
+                  margin: 0,
+                  borderWidth: 1,
+                }}
+              />
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+              >
+                <img
+                  src="http://localhost:3000/calendar.png" // Replace 'circular_image_url.jpg' with the URL of your circular image
+                  alt="Circular Image"
+                  style={{
+                    borderRadius: "50%", // Make the image circular
+                    border: "1px solid white", // Optional: add border to the circular image
+                    width: 60, // Adjust width and height as needed
+                    height: 60,
+                  }}
+                />
+              </div>
             </div>
-            <hr
-              style={{
-                width: "100%",
-                margin: 0,
-                borderWidth: 0.5,
-                borderColor: "white",
-              }}
-            />{" "}
             <div
               style={{
                 fontSize: 14,
@@ -100,7 +154,7 @@ const handleRequest = frames(async (ctx) => {
               color: "white",
             }}
           >
-            Shedule a call ?
+            Schedule
           </div>
         </div>
       ),
@@ -378,7 +432,87 @@ const handleRequest = frames(async (ctx) => {
         </Button>,
       ],
     };
+  } else if (booking["captcha"] === undefined) {
+    const input: GenerateCaptchaChallengeInput = {
+      options: { ratio: FrameRatio._1_91__1, includeImage: true },
+    };
+
+    const res: GenerateCaptchaChallengeOutput = await generateCaptchaChallenge(
+      input
+    );
+
+    // console.log(res);
+    // ctx.message!.state = res.state;
+    // console.log("state");
+    // console.log(ctx.message?.state);
+
+    return {
+      image: (
+        <div style={{ display: "flex" }}>
+          <img src={res.image}></img>
+        </div>
+      ),
+      state: {
+        captchaId: res.state.captchaId,
+        valueHash: res.state.valueHash,
+      },
+      // image: res.image,
+      buttons: [
+        <Button
+          action="post"
+          target={`/bookings?duration=${booking["duration"]}&d=${booking["d"]}&datefixed=true&t=${booking["t"]}&timefixed=true&captcha=pending&captchaId=${res.state.captchaId}&hashvalue=${res.state.valueHash}`}
+        >
+          Verify
+        </Button>,
+      ],
+      textInput: "Enter the answer",
+    };
+  } else if (booking["verified"] === undefined) {
+    const inputText = ctx.message?.inputText;
+    const states = ctx.message!.state;
+
+    const state = {
+      captchaId: booking["captchaId"],
+      valueHash: booking["hashvalue"],
+    };
+
+    console.log(`state : ${state}`);
+    const input: ValidateCaptchaChallengeInput = {
+      inputText,
+      state,
+    };
+
+    const res: ValidateCaptchaChallengeOutput = await validateCaptchaChallenge(
+      input
+    );
+
+    console.log(res);
+    return {
+      image: (
+        <div style={{ display: "flex" }}>
+          <img src={res.image}></img>
+        </div>
+      ),
+      // image: res.image,
+      buttons: [
+        <Button
+          action="post"
+          target={
+            res.isValidated
+              ? `/bookings?duration=${booking["duration"]}&d=${booking["d"]}&datefixed=true&t=${booking["t"]}&timefixed=true&captcha=pending&verified=true`
+              : `/bookings?duration=${booking["duration"]}&d=${booking["d"]}&datefixed=true&t=${booking["t"]}&timefixed=true`
+          }
+        >
+          {res.isValidated ? "Proceed" : "Try again!"}
+        </Button>,
+      ],
+    };
   } else {
+    const t = booking["t"].toString();
+    const d = booking["d"].toString();
+
+    const dur = booking["duration"].toString();
+
     return {
       image: (
         <div
@@ -468,20 +602,24 @@ const handleRequest = frames(async (ctx) => {
           <div
             style={{
               color: "white",
+              display: "flex", // flex must be there or it throws error
+              fontSize: 14,
             }}
           >
-            Shedule a call ?
+            You have booked a {dur} min appointment on {d} at {t}
           </div>
         </div>
       ),
       buttons: [
-        <Button action="post" target="/bookings?duration=30">
-          30min
+        <Button
+          action="post"
+          target={`/bookings?duration=${booking["duration"]}&d=${booking["d"]}&datefixed=true&t=${booking["t"]}`}
+        >
+          back
         </Button>,
-        <Button action="post" target="/bookings?duration=15">
-          15min
+        <Button action="tx" target="/txdata" post_url="/bookings">
+          Book Now
         </Button>,
-        <Button action="post_redirect">Create shedule</Button>,
       ],
     };
   }
