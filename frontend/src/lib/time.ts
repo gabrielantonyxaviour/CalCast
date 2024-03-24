a8daf41a8a44a18931079cf80a64cdba0fb6d7d9
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2024-03-24 21:31:31 +0530

    time changes

--- frontend/src/lib/time.ts
+++ frontend/src/lib/time.ts
@@ -14,11 +14,13 @@ export function createTimeSlots(startTime: string, endTime: string): string[] {
 
 function convertSecondsToTime(seconds: number): string {
   return (
-    Math.floor(seconds / 60)
+    Math.floor(seconds / 3600)
       .toString()
       .padStart(2, "0") +
     ":" +
-    (seconds % 60).toString().padStart(2, "0") +
+    Math.floor(seconds / 60)
+      .toString()
+      .padStart(2, "0") +
     seconds
   );
 }
