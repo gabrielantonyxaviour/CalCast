4238f52af97222d8e26eeec43b7424ecb03fa834
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-03-24 21:24:39 +0530

    add function to create time slot

--- frontend/src/lib/time.ts
+++ frontend/src/lib/time.ts
@@ -1,25 +1,26 @@
 export function createTimeSlots(startTime: string, endTime: string): string[] {
   const slots: string[] = [];
-  let currentTime = startTime;
-
-  while (currentTime < endTime) {
-    const nextTime = add30Minutes(currentTime);
-    slots.push(`${currentTime} - ${nextTime}`);
-    currentTime = nextTime;
+  var a = startTime.split(":"); // split it at the colons
+  let startTimeInSeconds = +a[0] * 60 * 60 + +a[1] * 60;
+  let currentTime = startTimeInSeconds;
+  var b = endTime.split(":"); // split it at the colons
+  let endTimeInSeconds = +b[0] * 60 * 60 + +b[1] * 60;
+  for (let i = startTimeInSeconds; i <= endTimeInSeconds; i += 900) {
+    slots.push(convertSecondsToTime(i));
   }
 
   return slots;
 }
 
-function add30Minutes(time: string): string {
-  const [hours, minutes] = time.split(":").map(Number);
-  const totalMinutes = hours * 60 + minutes + 30;
-  const newHours = Math.floor(totalMinutes / 60);
-  const newMinutes = totalMinutes % 60;
-  return `${padWithZero(newHours)}:${padWithZero(newMinutes)}`;
-}
-function padWithZero(num: number): string {
-  return num < 10 ? `0${num}` : `${num}`;
+function convertSecondsToTime(seconds: number): string {
+  return (
+    Math.floor(seconds / 60)
+      .toString()
+      .padStart(2, "0") +
+    ":" +
+    (seconds % 60).toString().padStart(2, "0") +
+    seconds
+  );
 }
 
 export function generateTimeSlots(startTime: string, endTime: string): string {
